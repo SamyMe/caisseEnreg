213ac416b99700d95da7d2812d6c401b4cb52cce
samy <samy@clustaar.com>
2017-02-21 16:14:42 +0100

    [sprint 1] deplacer data dans test_data/, ajout calculate_total

--- caisse.py
+++ caisse.py
@@ -1,7 +1,7 @@
 
 class caisse(object):
     
-    def __init__(self, catalogue_file="catalogue.csv"):
+    def __init__(self, catalogue_file="test_data/catalogue.csv"):
         self.catalogue_file=catalogue_file
         self.catalogue = {}
         self._lire_catalogue()
@@ -15,7 +15,6 @@ class caisse(object):
 
         print('\n* Lecture du catalogue...')
         with open(self.catalogue_file, 'r') as cat_file:
-            i = 0
             produit = ''
             prix = ''
             for row in cat_file.readlines():
@@ -37,9 +36,47 @@ class caisse(object):
                     print("[ERREUR CATALOGUE]: Produit '{}' dupliqué!".format(produit))
 
 
+    def calculate_total(self,):
+        
+        print('\n* Début de saisie...')
+        print("""
+                Veuillez entrer les produits sous la forme:
+                NomDuProduit  NombreDeKilo
+                et taper fin pour terminer la saisie.
+               """)
+
+        end_saisie = False
+        total_achat = 0
+        i = 1
+
+        # Boucler sur l'introduction des articles
+        while not end_saisie==True:
+            saisie = input('Article n°{}: '.format(i))
+
+            if 'fin' == saisie.lower():
+                end_saisie = True
+                print("Total: {:.2f}€".format(total_achat))
+            else:
+                saisie = saisie.split(',')
+
+                try:
+
+                    produit = saisie[0]
+                    quantite = saisie[1]
+                    prix = self.catalogue[produit]
+
+                    total_achat += float(quantite)*prix 
+
+                except ValueError:
+                    print("[ERREUR]: Quantité mal introduite!!")
+                except IndexError:
+                    print("[ERREUR]: Saisie erronée!")
+                except KeyError:
+                    print("[ERREUR]: Produit inconue!")
+
             i += 1
 
 
-
 if __name__ == "__main__":
-	c = caisse()
+    c = caisse()
+    c.calculate_total()
